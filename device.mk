62fd7d75fffb84b2592b24e21dafb768aefd0acf
arixcute <125747568+arixcuti@users.noreply.github.com>
2023-03-11 21:37:56 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -7,9 +7,9 @@ PRODUCT_PACKAGES += \
     qcom_decrypt \
     qcom_decrypt_fbe
 
+# Dependencies
 TARGET_RECOVERY_DEVICE_MODULES += \
-    libion \
-    libandroidicu
+    libion
 
 RECOVERY_LIBRARY_SOURCE_FILES += \
     $(TARGET_OUT_SHARED_LIBRARIES)/libion.so
